ee4f0a8cd5bf7db4d6ce8d3f5c565d852e717a7a
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-05-18 14:54:38 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+ #MANUAL ROUTE CREATION
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
+
+ #RAILS ROUTE CREATION IN-BUILT TOOL
+ #OneCodeLiner — resources :birds — Rails created all the RESTful routes we need and 
+ #mapped them to the appropriate controller action!
+ #resources :birds
+
+ resources :birds, only: [:index, :show]
 end
